c67205d058a97c3f889adf166bf5af300a165ff8
Mamadou <masaliou20142015@gmail.com>
2020-11-28 16:16:21 +0000

    test

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,6 +15,7 @@ Route::get('/', function () {
 //    return view('welcome');
     return view('auth.login');
 });
+
 
 Auth::routes();
 Route::group(['middleware' => ['auth']], function (){
